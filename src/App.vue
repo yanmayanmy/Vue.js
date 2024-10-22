807d3a8f0e9454adb92a74da81c04e9cc30134b9
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 17:43:32 +0900

    [Done]3.38 watch & watchEffect

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <script setup>
-import { ref, watchEffect } from 'vue';
-const count = ref(0)
-watchEffect(() => {
-  console.log('watchEffect')
-  console.log(count.value)
-  setTimeout(()=> {
-    console.log('after 1 second')
-  }, 1000)
-  count.value = 'hello'
+import { ref, watch, watchEffect } from 'vue';
+const count1 = ref(0)
+const count2 = ref({
+  a: 0,
+  b: 0
+})
+const count3 = ref(0)
+
+watch([count1, () => count2.value.a], (newVal, oldVal) => {
+  console.log('watch')
+  console.log('newVal', newVal)
+  console.log('oldVal', oldVal)
+  console.log(count1.value, count2.value, count3.value)
 })
 
-
+watchEffect(() => {
+  console.log('watchEffect')
+  console.log(count1.value, count2.value, count3.value)
+})
 </script>
 
 <template>
-  <p>{{ count }}</p>
-  <button @click="count++">+1</button>
+  <p>count1 {{ count1 }}</p>
+  <p>count2 {{ count2 }}</p>
+  <p>count3 {{ count3 }}</p>
+  <button @click="count1++">1</button>
+  <button @click="count2.a++">2</button>
+  <button @click="count3++">3</button>
 </template>
