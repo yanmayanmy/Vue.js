85a37f6f028289aed0ac6dd78cdb2a9a8859e3e7
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 16:18:07 +0900

    [Done]3.28 event argument

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,20 @@
 import { ref } from 'vue';
 
   const count = ref(0)
+  let x = ref(0)
+  let y = ref(0)
 
-  function countUp() {
-    count.value++
+  function countUp(event) {
+    console.log(event)
+    x.value = event.clientX
+    y.value = event.clientY
   }
 
 </script>
 <template>
   <p>{{ count }}</p>
-  <button v-on:click="count++">Count up</button>
+  <p>{{ x }}</p>
+  <p>{{ y }}</p>
+  <button v-on:click="count = $event.clientX">Count up</button>
   <button @click="countUp">Count up</button>
 </template>
