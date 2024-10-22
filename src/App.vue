bad8cf1a6205cd0209686676ca1da177557ee74a
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 16:43:13 +0900

    [Done]3.31 Key decoration

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,9 @@
 import { ref } from 'vue';
 
   const count = ref(0)
-  let x = ref(0)
-  let y = ref(0)
-
-  function countUp(event, times) {
-    count.value = count.value*times
-  }
 
 </script>
 <template>
   <p>{{ count }}</p>
-  <p>{{ x }}</p>
-  <p>{{ y }}</p>
-  <button v-on:click="count++">Count up</button>
-  <button @click="countUp($event, 5)">x5</button>
+  <input type="text" @keyup.a="count++">
 </template>
