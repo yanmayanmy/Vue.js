f88c1899c4819914dd2646b05a39077d9583f725
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 18:00:57 +0900

    [Done]3.40 styling

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <script setup>
 import { ref } from 'vue';
 
-const isRed = ref(true)
-const isBgBlue = ref(true)
-const className = ref('red')
-function toggleClass() {
-  isRed.value = !isRed.value
-  isBgBlue.value = !isBgBlue.value
-}
-
 </script>
 
 <template>
-  <div class="border" :class="[className, {'bg-blue': isBgBlue}]">Hello World</div>
-  <button @click="toggleClass">Change Style</button>
+  <div :style="{ color:'red', backgroundColor: 'blue'}">Hello World</div>
 </template>
 
 <style>
-.red {
-  color: red;
-}
-.bg-blue {
-  background-color: blue;
-}
-.border {
-  border: 1px solid red;
-}
+
 </style>
 
