5b307956a1382be4f44e1e1a5c209455b3689eb6
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 12:56:10 +0900

    [Done]1.12

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const title = "Vue.js Course"
+let price = 9.99
+</script>
+
+<template>
+  <h1>Title: {{ title }}</h1>
+  <h2>Price: {{ price }}</h2>
+</template>
+
+<style>
+/* h1 {
+  color: red;
+} */
+</style>
