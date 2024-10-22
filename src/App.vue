3d766890bac70f3856923389ba59379574405db2
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 16:47:42 +0900

    [Done]3.33 Dynamic argument

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 import { ref } from 'vue';
 
   const count = ref(0)
-
+  const eventName = 'keyup'
 </script>
 <template>
   <p>{{ count }}</p>
-  <input type="text" @keyup.a="count++">
+  <input type="text" @[eventName]="count++">
 </template>
