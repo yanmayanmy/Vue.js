fe547c4af3cd15a960c9d11a4ba890c89ccbff6a
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 17:24:31 +0900

    [Done]3.37 watchEffect

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
-import { ref, computed } from 'vue';
-  const score = ref(0)
-  const evaluate = computed(() => {
-    return score.value > 3 ? 'Good' : 'Bad'
-  })
-console.log(evaluate.value)
+import { ref, watchEffect } from 'vue';
+const count = ref(0)
+watchEffect(() => {
+  console.log('watchEffect')
+  console.log(count.value)
+  setTimeout(()=> {
+    console.log('after 1 second')
+  }, 1000)
+  count.value = 'hello'
+})
+
+
 </script>
 
 <template>
-  <p>{{ score }}</p>
-  <p>{{ evaluate }}</p>
-  <p>{{ score > 3 ? 'Good' : 'Bad' }}</p>
-  <button @click="score++">Button</button>
+  <p>{{ count }}</p>
+  <button @click="count++">+1</button>
 </template>
