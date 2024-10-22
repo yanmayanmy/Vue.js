4510490291d07c252cfe6d0a537224c648bfb76f
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 14:28:48 +0900

    [Done]1.15

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <script setup>
+import { ref } from 'vue'
+
 const title = "Vue.js Course"
-let price = 9.99
+let price = ref(9.99)
+const info = ref({
+  students: 400,
+  rating: 10
+})
+
+console.log(info.value.students)
+console.log(info.value.rating)
+
 function increment() {
-  price += 1
+  price.value += 1
   console.log(price)
+  console.log(price.value)
 }
 </script>
 
 <template>
   <h1>Title: {{ title }}</h1>
   <h2>Price: {{ price }}</h2>
+<p>students:{{ info.students }}</p>
+<p>rating:{{ info.rating }}</p>
   <button @click="increment">button</button>
 </template>
 
