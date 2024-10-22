3e0d82512d25dd176e4cc5eb1566e87d71fbfd07
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 15:39:17 +0900

    [Done]3.24 v-bind

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
 import { ref } from 'vue';
 
 const message = ref("<h1>hello</h1>")
+const googleURL = ref('https://google.com')
 </script>
 <template>
   <div>{{ message }}</div>
+  <a :href="googleURL">Google</a>
   <div v-html='message'></div>
 </template>
