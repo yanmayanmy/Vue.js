6eadad4077d4cad00bdb7a4d61e969d42dba4b44
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 12:59:45 +0900

    [Done]1.13

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 const title = "Vue.js Course"
 let price = 9.99
+function increment() {
+  price += 1
+  console.log(price)
+}
 </script>
 
 <template>
   <h1>Title: {{ title }}</h1>
   <h2>Price: {{ price }}</h2>
+  <button @click="increment">button</button>
 </template>
 
 <style>
