ad084648e0891813cdd9ec118e3408659b53f6dd
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-22 15:43:40 +0900

    [Done]3.25 boolean attr

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-import { ref } from 'vue';
 
-const message = ref("<h1>hello</h1>")
-const googleURL = ref('https://google.com')
 </script>
 <template>
-  <div>{{ message }}</div>
-  <a :href="googleURL">Google</a>
-  <div v-html='message'></div>
+  <button :disabled="false">false</button>
+  <br>
+  <button :disabled="true">true</button>
+  <br>
+  <button :disabled="undefined">true</button>
+  <br>
+  <button :disabled="NaN">true</button>
+  <br>
+  <button :disabled="foo">true</button>
+  <br>
+  <button :disabled="''">true</button>
 </template>
