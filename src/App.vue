a0e486f9760ca730d45adf5db7630b272ad36ed1
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-23 16:04:49 +0900

    [add] 5.68 emit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from 'vue'
 import ShowCount from './components/ShowCount.vue'
+import ResetButton from './components/ResetButton.vue'
 const count = ref(1)
 </script>
 
 <template>
   <ShowCount :total-score="count" />
   <button @click="count++">+1</button>
+  <ResetButton @reset="count = 0" />
 </template>
