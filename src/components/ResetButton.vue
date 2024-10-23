9f09cca7b812ca3bdd27d59e3c98601adf74b118
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-23 16:15:41 +0900

    [add] 5.70 defineEmits

--- src/components/ResetButton.vue
+++ src/components/ResetButton.vue
@@ -1,3 +1,10 @@
+<script setup>
+const emit = defineEmits(['reset'])
+function emitReset() {
+  emit('reset', 100)
+}
+</script>
+
 <template>
-  <button @click="$emit('reset', 100)">Reset</button>
+  <button @click="emitReset">Reset</button>
 </template>
