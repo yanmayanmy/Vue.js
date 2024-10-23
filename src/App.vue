e8ba5b730f9a95a2fff12ac963ebac913edbb495
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-23 09:46:38 +0900

    [add]4.47 Getting index number from for loop.

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,9 @@ const fruits = ref([
 <template>
   <ul>
     <button @click="fruits.shift()">Delete</button>
-    <li v-for="fruit in fruits" :key="fruit.id">
+    <li v-for="(fruit, index) in fruits" :key="fruit.id">
       <input type="text" />
+      {{ index }}:
       {{ fruit.name }}
     </li>
   </ul>
