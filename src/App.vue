7d5165bc6da572ba2851e7e5d895503203f74b16
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-24 10:24:43 +0900

    [add]9.89 Dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,16 @@
 <script setup>
-import BaseCard from './components/BaseCard.vue'
-import OneSlot from './components/OneSlot.vue'
+import { ref } from 'vue'
+import CompA from './components/CompA.vue'
+import CompB from './components/CompB.vue'
+import CompC from './components/CompC.vue'
+
+const currentComp = ref(CompA)
 </script>
 
 <template>
-  <h1>slots</h1>
-  <BaseCard>
-    <template v-slot:header="slotProps">
-      <p>slotProps: {{ slotProps }}</p>
-      <p>pageCount: {{ slotProps.pageCount }}</p>
-      <p>foo: {{ slotProps.foo }}</p>
-      <h2>HTML</h2>
-    </template>
-    <template v-slot:main>
-      <p>hello world</p>
-    </template>
-    <template #footer> </template>
-  </BaseCard>
-  <BaseCard>
-    <template v-slot:header="{ pageCount }">
-      <h2 v-if="pageCount === 1">HTML</h2>
-      <h2 v-if="pageCount === 2">CSS</h2>
-      <h2 v-if="pageCount === 3">PHP</h2>
-    </template>
-    <template v-slot:main>
-      <p>hello world</p>
-    </template>
-    <template #footer> </template>
-  </BaseCard>
-  <!-- When you have only one slot in your children componet, you can simply add v-slot attribute in the tag -->
-  <OneSlot v-slot="{ pageCount }">
-    <h2 v-if="pageCount === 1">HTML</h2>
-    <h2 v-if="pageCount === 2">CSS</h2>
-    <h2 v-if="pageCount === 3">PHP</h2>
-    <p>hello world</p>
-  </OneSlot>
+  <h1>Dynamic component</h1>
+  <button @click="currentComp = CompA">A</button>
+  <button @click="currentComp = CompB">B</button>
+  <button @click="currentComp = CompC">C</button>
+  <component :is="currentComp"></component>
 </template>
---
+++ src/components/CompC.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h2>component C</h2>
+  </div>
+</template>
+
+<style scoped>
+div {
+  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.4);
+  border-radius: 5px;
+  padding: 16px;
+}
+</style>
