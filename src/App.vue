d6d8c79acea5575ddf10f853e552d249369ba682
yuta.yamauchi <yuta.yamauchi@accenture.com>
2024-10-24 13:50:06 +0900

    [add]10.99 v-model & checkbox

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import { ref } from 'vue'
 
 const userInput = ref('')
 const message = ref('')
+const checked = ref('not checked')
+const fruits = ref([])
 </script>
 
 <template>
@@ -13,4 +15,31 @@ const message = ref('')
   <h2>Textarea</h2>
   <textarea v-model="message"></textarea>
   <p style="white-space: pre">{{ message }}</p>
+  <h2>Check box</h2>
+  <input
+    id="checkbox"
+    v-model="checked"
+    type="checkbox"
+    true-value="checked"
+    false-value="not checked"
+  />
+  <label for="checkbox">{{ checked }}</label>
+  <p>Fruits</p>
+  <input v-model="fruits" id="Apple" type="checkbox" value="Apple is checked" />
+  <label for="Apple">Apple</label>
+  <input
+    v-model="fruits"
+    id="Banana"
+    type="checkbox"
+    value="Banana is checked"
+  />
+  <label for="Banana">Banana</label>
+  <input
+    v-model="fruits"
+    id="Grape"
+    type="checkbox"
+    value="Grapes is checked"
+  />
+  <label for="Grape">Grape</label>
+  <p>{{ fruits }}</p>
 </template>
